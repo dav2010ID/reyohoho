a2500a56ea49020d7f204a2a6365508a999559ff
dav2010ID <dav2010ID@users.noreply.github.com>
2026-03-01 12:44:39 +0000

    deploy: 9c8fdfd4ced8fabaf087bfc92f34761d795b7157

---
+++ assets/trailer-Ysc_Vp44.js
@@ -0,0 +1,1 @@
+import{aJ as e}from"./index-D6L3eJ-R.js";const t=e("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0});export{t as u};
